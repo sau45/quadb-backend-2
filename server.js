dce359cdfc2fcc7a22e9efaab16d375174fa698a
Saurav Bharti <sauravbharti38@gmail.com>
2024-02-24 00:35:34 +0530

    minor change

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/api/tickers/data', async(req,res)=>{
     
     try {
         const data= await pool.query("SELECT * FROM ticker_data ;");
-        console.log(data.rows)
+        // console.log(data.rows)
         res.json({status:true,data : data.rows});
     } catch (error) {
         throw error;
